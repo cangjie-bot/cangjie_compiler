2055491e458ac430210a27eeba347d7abb9589dd
JasmineZhu <zhujiameng@huawei.com>
2025-10-19 13:31:22 +0300

    feat: sync add features of CJMP/FFI-Java/FFI-ObjC
    Signed-off-by: JasmineZhu <zhujiameng@huawei.com>

--- include/cangjie/Option/Option.h
+++ include/cangjie/Option/Option.h
@@ -1124,6 +1124,11 @@ public:
         return emitCHIRPhase != CandidateEmitCHIRPhase::NA;
     }
 
+    bool IsCompilingCJMP() const
+    {
+        return inputChirFiles.size() > 0;
+    }
+
 protected:
     virtual std::optional<bool> ParseOption(OptionArgInstance& arg);
     virtual bool PerformPostActions();
