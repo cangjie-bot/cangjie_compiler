9fbfb54a62451a873c99edecad30ffe1057938cc
Tadddy <yinkejie@huawei.com>
2025-10-22 17:10:09 +0800

    bugfix: solve chir devirt bug with const member
    Signed-off-by: Tadddy <yinkejie@huawei.com>

--- include/cangjie/CHIR/Utils.h
+++ include/cangjie/CHIR/Utils.h
@@ -738,5 +738,23 @@ bool ParamTypeIsEquivalent(const Type& paramType, const Type& argType);
  * @return type with vtable.
  */
 BuiltinType* GetBuiltinTypeWithVTable(BuiltinType& type, CHIRBuilder& builder);
+
+/**
+ * @brief Try to get instantiated sub type.
+ *
+ * @param genericSubType generic sub type.
+ * @param instParentType instantiated parent type
+ * @param builder CHIR builder
+ * @return return genericSubType when
+ *  1. `genericSubType` is not generic related
+ *  2. `genericSubType` equals to `instParentType`
+ *  3. `genericSubType` and `instParentType` don't have parent-child relationship
+ *  return instantiated sub type when
+ *  1. `genericSubType` and `instParentType` are same CustomTypeDef,
+ *     but one is generic related, the other is instantiated type
+ *  2. `genericSubType` and `instParentType` have parent-child relationship,
+ *     and `genericSubType` is generic related sub type, `instParentType` is instantiated parent type
+ */
+Type* GetInstSubType(Type& genericSubType, const Type& instParentType, CHIRBuilder& builder);
 } // namespace Cangjie::CHIR
 #endif
