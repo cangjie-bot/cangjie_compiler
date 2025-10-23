844cb613afabca2a3a24a5793d8fed7bdbab1a27
kkkkkkkbbbbbbb <songjian59@huawei.com>
2025-10-23 07:34:48 +0000

    feat: add stdx.chir
    Signed-off-by: kkkkkkkbbbbbbb <songjian59@huawei.com>

--- include/cangjie/CHIR/Serializer/CHIRSerializer.h
+++ include/cangjie/CHIR/Serializer/CHIRSerializer.h
@@ -10,16 +10,31 @@
 #include "cangjie/CHIR/CHIR.h"
 #include "cangjie/CHIR/Package.h"
 
-#include <string>
-
+#ifdef CANGJIE_CHIR_PLUGIN
+namespace flatbuffers {
+    class DetachedBuffer;
+}
+#endif
 namespace Cangjie::CHIR {
+#ifdef CANGJIE_CHIR_PLUGIN
+struct DetachedBuffer {
+    DetachedBuffer(class ::flatbuffers::DetachedBuffer&& buf);
+    ~DetachedBuffer();
+
+    void* Data();
+    size_t Size();
+private:
+    struct DetachedBufferImpl* impl;
+};
+#endif
 
 class CHIRSerializer {
     class CHIRSerializerImpl;
 public:
     static void Serialize(const Package& package, const std::string filename, ToCHIR::Phase phase);
+#ifdef CANGJIE_CHIR_PLUGIN
+    static DetachedBuffer Serialize(const Package& package, ToCHIR::Phase phase);
+#endif
 };
-
 } // namespace Cangjie::CHIR
-
 #endif
