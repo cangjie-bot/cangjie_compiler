70ce59f6f77280fee604ad260115537864750d0d
Dmitrii Chizha <chizha.dmitrii@huawei.com>
2025-10-24 14:58:17 +0000

    feat(native-ffi-objc): objective-c native-ffi enhancements
    Signed-off-by: Dmitrii Chizha <chizha.dmitrii@huawei.com>

--- src/Sema/NativeFFI/ObjC/AfterTypeCheck/Interop/CheckMirrorTypes.cpp
+++ src/Sema/NativeFFI/ObjC/AfterTypeCheck/Interop/CheckMirrorTypes.cpp
@@ -18,9 +18,9 @@ using namespace Cangjie::Interop::ObjC;
 
 void CheckMirrorTypes::HandleImpl(InteropContext& ctx)
 {
-    auto checker = HandlerFactory<TypeCheckContext>::Start<CheckInterface>()
-                       .Use<CheckAbstractClass>()
+    auto checker = HandlerFactory<TypeCheckContext>::Start<CheckAbstractClass>()
                        .Use<CheckMirrorInheritMirror>()
+                       .Use<CheckForeignName>()
                        .Use<CheckMemberTypes>(InteropType::ObjC_Mirror);
 
     for (auto& mirror : ctx.mirrors) {
