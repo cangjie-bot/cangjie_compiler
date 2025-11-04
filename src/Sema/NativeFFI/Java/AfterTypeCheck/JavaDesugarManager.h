9cebfd9ae49e01cc3d5c92b2072a1abc4350710c
Mark-chen <chenxuesheng1@huawei.com>
2025-11-04 10:18:05 +0800

    feat: support struct/class generic for java call cangjie
    Signed-off-by: Mark-chen <chenxuesheng1@huawei.com>

--- src/Sema/NativeFFI/Java/AfterTypeCheck/JavaDesugarManager.h
+++ src/Sema/NativeFFI/Java/AfterTypeCheck/JavaDesugarManager.h
@@ -283,7 +283,7 @@ private:
     bool FillMethodParamsByArg(std::vector<OwnedPtr<FuncParam>>& params, std::vector<OwnedPtr<FuncArg>>& callArgs,
         FuncDecl& funcDecl, OwnedPtr<FuncParam>& arg, FuncParam& jniEnvPtrParam);
 
-    OwnedPtr<Decl> GenerateNativeMethod(FuncDecl& sampleMethod, Decl& decl);
+    OwnedPtr<Decl> GenerateNativeMethod(FuncDecl& sampleMethod, Decl& decl, std::string* actualType = nullptr);
 
     void GenerateFuncParamsForNativeDeleteCjObject(
         Decl& decl, std::vector<OwnedPtr<FuncParam>>& params, FuncParam*& jniEnv, OwnedPtr<Expr>& selfRef);
@@ -487,7 +487,8 @@ private:
     void DesugarSuperMethodCall(CallExpr& call, ClassDecl& impl);
 
     void GenerateInJavaImpl(AST::ClassDecl* classDecl);
-    void GenerateForCJStructOrClassTypeMapping(const File &file, AST::Decl* decl);
+    void GenerateForCJStructOrClassTypeMapping(const File &file, AST::Decl* decl,
+        std::unordered_map<std::string, std::unordered_map<std::string, std::vector<std::string>>>* declActualMap = nullptr);
     void GenerateForCJEnumMapping(AST::EnumDecl& enumDecl);
     void GenerateForCJExtendMapping(AST::ExtendDecl& extendDecl);
 
@@ -519,6 +520,7 @@ private:
      */
     void GenerateForCJInterfaceMapping(AST::InterfaceDecl& interfaceDecl);
     void GenerateNativeForCJInterfaceMapping(AST::ClassDecl& classDecl);
+    void GenerateForCJGenericTypeMapping(const File& file, AST::Decl* decl);
     void GenerateInterfaceFwdclassBody(AST::ClassDecl& fwdclassDecl, AST::InterfaceDecl& interfaceDecl);
     OwnedPtr<FuncDecl> GenerateInterfaceFwdclassMethod(AST::ClassDecl& fwdclassDecl, FuncDecl& interfaceFuncDecl);
     OwnedPtr<FuncDecl> GenerateInterfaceFwdclassDefaultMethod(
