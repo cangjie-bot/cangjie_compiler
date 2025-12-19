6815ea387cc4ef8614d20ce1dbc1e99bcd23c23c
Augus_Jan <zhanguozheng.829268@huawei.com>
2025-12-19 19:14:03 +0800

    fix: separate cjdPath from cjoPath
    Signed-off-by: Augus_Jan <zhanguozheng.829268@huawei.com>

--- include/cangjie/Modules/ImportManager.h
+++ include/cangjie/Modules/ImportManager.h
@@ -341,6 +341,11 @@ public:
         return cjoFilePaths;
     }
 
+    std::unordered_map<std::string, std::string> GetDepPkgCjdPaths()
+    {
+        return cjdFilePaths;
+    }
+
     DiagnosticEngine& GetDiagnosticEngine() const
     {
         return diag;
@@ -473,8 +478,10 @@ private:
     Ptr<AST::Package> curPackage;
     TypeManager& typeManager;
 
-    // Key is cjd fullPackageName, and value is .cjo file path.
+    // Key is fullPackageName, and value is .cjo file path.
     std::unordered_map<std::string, std::string> cjoFilePaths;
+    // Key is fullPackageName, and value is .cjd file path.
+    std::unordered_map<std::string, std::string> cjdFilePaths;
 };
 } // namespace Cangjie
 #endif // CANGJIE_MODULES_IMPORTMANAGER_H
