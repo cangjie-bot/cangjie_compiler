7a9963c2b48a45ca755b870869d5641e3e2cc0f1
jinbohang <jinbohang1@huawei.com>
2026-01-10 21:56:59 +0800

    feat: support obj
    Signed-off-by: jinbohang <jinbohang1@huawei.com>

--- src/Option/Option.cpp
+++ src/Option/Option.cpp
@@ -557,7 +557,7 @@ bool GlobalOptions::CheckOutputModeOptions() const {
         diag.DiagnoseRefactor(DiagKindRefactor::driver_invalid_compile_target, DEFAULT_POSITION);
         return false;
     }
-    if(srcFiles.empty() && outputMode == OutputMode::OBJ){
+    if(srcFiles.empty() && (outputMode == OutputMode::OBJ || outputMode == OutputMode::CHIR)){
         DiagnosticEngine diag;
         diag.DiagnoseRefactor(DiagKindRefactor::driver_source_file_empty, DEFAULT_POSITION);
         return false;
